e47b00b940863a8489422aa9536a494c63a7abfd
Ian Baldwin <iantbaldwin@elektrikfish.com>
2014-11-19 01:49:30 -0500

    Finshied TO-DO list left from last night. Script functions to create environment neccesary to store IP address information. Refer to README to understand full usage of file.

--- IPSync.sh
+++ IPSync.sh
@@ -7,40 +7,72 @@
 # And notifies you if it has (via Pushover)!
 
 ## Constants
-# Modify only if necessary
-FILENAME=IP.sync # Name of the database file
-DEVICENAME=ApachePi # Local Device Name (Should contain no whitespace)
-DELIMITER=" | " # Delimiter used between the device name and the IP Adress
-NOTIFY=true
+DATABASE=IPSync.db
+NAMEFILE=device.name
+if [ ! -s $NAMEFILE ]; then
+	echo "Namefile required. Enter device name (one word, no spaces): "
+	read devName
+	echo $devName > $NAMEFILE
+fi
+DEVICENAME=$(cut -d' ' -f1 $NAMEFILE)
+DELIMITER=":"
 
-##<------- DO NOT MODIFY  ------>##
-# Checks IP Adress with outside resource, icanhazip.com
-curIP=$(curl -s icanhazip.com)
+# Checks IP Adress with external resource, icanhazip.com
+curIP=$(wget -O - -q icanhazip.com)
 
-# Checks to see if the IP database file exists (1 if true, 0 if false)
-if [ ! -s $FILENAME ]; then
-	touch $FILENAME
+# Checks to see if the IP database file exists
+# Creates it if it does not
+if [ ! -s $DATABASE ]; then
+	touch $DATABASE
 fi
 
 # Count Number of devices in file
 devCount=0
 while read line; do
 	((devCount++))
-done < $FILENAME
+done < $DATABASE
 
-# Create lists of devices and IP Address
+# Create array lists of device names and IP addresses
 curLine=0
 while read line; do
 	lineArray=($line)
 	deviceList[$curLine]=${lineArray[0]}
-	ipList[$curLine]=${lineArray[2]}}
+	ipList[$curLine]=${lineArray[2]}
 	((curLine++))
-done < $FILENAME
+done < $DATABASE
+unset curLine
 
-# Checks if device is one of the ones listed in the file
+# Checks if device is one of the ones listed in the file and
+# records the device number if it does, else it creates
+# the listing in the database file
+deviceNumber=-1
+i=0
 if [[ " ${lineArray[*]} " != *" $DEVICENAME "* ]]; then
-	ipInfo=$DEVICENAME$DELIMITER$curIP
-	echo $ipInfo >> $FILENAME
+	deviceList[$((devCount))]=$DEVICENAME
+	ipList[$((devCount))]=$curIP
+	((devCount++))
+else
+	while [ $i -lt $devCount ]; do
+		if [[ "${deviceList[$i]}" == "$DEVICENAME" ]]; then
+			deviceNumber=$i
+		fi
+		((i++))
+	done
+fi
+unset i
+
+# Change the corresponding IP Address if different
+if [ ${ipList[$deviceNumber]} != $curIP ]; then
+	echo "IP Address changed from ${ipList[$deviceNumber]} to $curIP"
+	ipList[$deviceNumber]=$curIP
 fi
 
-## TO DO: If devices exists but IPs differ, replace IP and call notification script (if needed)
+# Build the formated array
+i=0
+while [ $i -lt $devCount ]; do
+	outputArray[$i]="${deviceList[$i]} $DELIMITER ${ipList[$i]}"
+	((i++))
+done
+
+# Output changes to file
+printf "%s\n" "${outputArray[@]}" > $DATABASE
